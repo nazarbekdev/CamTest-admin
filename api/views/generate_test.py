85d679e345d179aaeeb25acd19aad3606faf8f0a
Nazarbek-Kabulov <nqobulov571@gmail.com>
2024-03-21 23:11:45 +0500

    updated generate-test api-> limit reduction

--- api/views/generate_test.py
+++ api/views/generate_test.py
@@ -10,7 +10,7 @@ from api.functions.merge_pdf import merge_pdf
 from api.functions.placement_of_numbers import placement_of_numbers
 from api.functions.random_name import generate_random_name
 from api.functions.revome_file import revome_file
-from api.models import SubjectTest, Subject, AnswerTest, UserFile
+from api.models import SubjectTest, Subject, AnswerTest, UserFile, CTUser
 from api.serializers import TestGeneratePDFSerializer
 
 
@@ -63,89 +63,97 @@ class GenerateTest(GenericAPIView):
         full_answers = []
         start_index = 0
         end_index = 90
-        for num in range(number_books):
-            for subject_name, tests, numbering_range in zip(subject_list, subjects, numbering_ranges):
-                self.add_subject_heading(elements, subject_name)
 
-                for i, test in enumerate(tests):
-                    test_text = f"{numbering_range[i]}. {test.question.replace('@', '')}\n\n" if '@' in test.question else f"{numbering_range[i]}. {test.question}\n\n"
-                    elements.append(Spacer(1, 5))
-                    elements.append(Paragraph(test_text, custom_style))
-                    if test.image:
-                        print('test image path:   ', test.image)
-                        test_image = Image(f'/home/nazarbek/CamTest-admin/{test.image}', width=110, height=70)
-                        elements.append(test_image)
-                        elements.append(Spacer(1, 20))
+        user_limit = CTUser.objects.get(id=user_name)
 
-                    len_ = len(test.answers) - 1
-                    tests_ = test.answers[:len_]
-                    shuffled_answers = random.sample(tests_.split('#'), len(tests_.split('#')))
-                    if shuffled_answers[0] == test.answer:
-                        f_a = {
-                            numbering_range[i]: 'A'
-                        }
-                        full_answers.append(f_a)
-                    elif shuffled_answers[1] == test.answer:
-                        f_a = {
-                            numbering_range[i]: 'B'
-                        }
-                        full_answers.append(f_a)
-                    elif shuffled_answers[2] == test.answer:
-                        f_a = {
-                            numbering_range[i]: 'C'
-                        }
-                        full_answers.append(f_a)
-                    elif shuffled_answers[3] == test.answer:
-                        f_a = {
-                            numbering_range[i]: 'D'
-                        }
-                        full_answers.append(f_a)
-                    else:
-                        f_a = {
-                            numbering_range[i]: 'Null'
-                        }
-                        full_answers.append(f_a)
-                    answers_str = f"A) {shuffled_answers[0]}\n\nB) {shuffled_answers[1]}\n\nC) {shuffled_answers[2]}\n\nD) {shuffled_answers[3]}\n\n"
+        if user_limit.limit >= number_books:
+            user_limit.limit -= number_books
+            user_limit.save()
+            for num in range(number_books):
+                for subject_name, tests, numbering_range in zip(subject_list, subjects, numbering_ranges):
+                    self.add_subject_heading(elements, subject_name)
 
-                    elements.append(Paragraph(answers_str, custom_style))
-                    # elements.append(Spacer(1, 5))
-                elements.append(Spacer(1, 20))
+                    for i, test in enumerate(tests):
+                        test_text = f"{numbering_range[i]}. {test.question.replace('@', '')}\n\n" if '@' in test.question else f"{numbering_range[i]}. {test.question}\n\n"
+                        elements.append(Spacer(1, 5))
+                        elements.append(Paragraph(test_text, custom_style))
+                        if test.image:
+                            print('test image path:   ', test.image)
+                            test_image = Image(f'/home/nazarbek/CamTest-admin/{test.image}', width=110, height=70)
+                            elements.append(test_image)
+                            elements.append(Spacer(1, 20))
 
-            pdf.build(elements)
+                        len_ = len(test.answers) - 1
+                        tests_ = test.answers[:len_]
+                        shuffled_answers = random.sample(tests_.split('#'), len(tests_.split('#')))
+                        if shuffled_answers[0] == test.answer:
+                            f_a = {
+                                numbering_range[i]: 'A'
+                            }
+                            full_answers.append(f_a)
+                        elif shuffled_answers[1] == test.answer:
+                            f_a = {
+                                numbering_range[i]: 'B'
+                            }
+                            full_answers.append(f_a)
+                        elif shuffled_answers[2] == test.answer:
+                            f_a = {
+                                numbering_range[i]: 'C'
+                            }
+                            full_answers.append(f_a)
+                        elif shuffled_answers[3] == test.answer:
+                            f_a = {
+                                numbering_range[i]: 'D'
+                            }
+                            full_answers.append(f_a)
+                        else:
+                            f_a = {
+                                numbering_range[i]: 'Null'
+                            }
+                            full_answers.append(f_a)
+                        answers_str = f"A) {shuffled_answers[0]}\n\nB) {shuffled_answers[1]}\n\nC) {shuffled_answers[2]}\n\nD) {shuffled_answers[3]}\n\n"
 
-            data_muqova = placement_of_numbers()
-            muqova = data_muqova[0]
-            test = 'subject_test.pdf'
+                        elements.append(Paragraph(answers_str, custom_style))
+                        # elements.append(Spacer(1, 5))
+                    elements.append(Spacer(1, 20))
 
-            def merge_pdfs(input_paths, output_path):
-                pdf_writer = PdfWriter()
+                pdf.build(elements)
 
-                for path in input_paths:
-                    with open(path, 'rb') as file:
-                        pdf_reader = PdfReader(file)
-                        for page in pdf_reader.pages:
-                            pdf_writer.add_page(page)
+                data_muqova = placement_of_numbers()
+                muqova = data_muqova[0]
+                test = 'subject_test.pdf'
 
-                with open(output_path, 'wb') as output_file:
-                    pdf_writer.write(output_file)
+                def merge_pdfs(input_paths, output_path):
+                    pdf_writer = PdfWriter()
 
-            all_tests.append(f'camtest-{subject_name1}-{subject_name2}{num+1}.pdf')
+                    for path in input_paths:
+                        with open(path, 'rb') as file:
+                            pdf_reader = PdfReader(file)
+                            for page in pdf_reader.pages:
+                                pdf_writer.add_page(page)
 
-            # Fayllarni birlashtirish va yangi faylni saqlash
-            merge_pdfs([muqova, test], f'camtest-{subject_name1}-{subject_name2}{num+1}.pdf')
-            os.remove(file_path)
+                    with open(output_path, 'wb') as output_file:
+                        pdf_writer.write(output_file)
 
-            book_code = data_muqova[1]
-            answer = AnswerTest(book_code=book_code, answers=full_answers[start_index:end_index])
-            answer.save()
-            start_index += 90
-            end_index += 90
+                all_tests.append(f'camtest-{subject_name1}-{subject_name2}{num+1}.pdf')
 
-        # barcha generatsiya bo'lgan test fayllarini birlashtirish
-        test_book_name = generate_random_name()
-        merge_pdf(all_tests, f'/home/nazarbek/CamTest-admin/tests/{test_book_name}.pdf')
-        data = UserFile(user_id=user_name, file=f'{test_book_name}.pdf')
-        data.save()
+                # Fayllarni birlashtirish va yangi faylni saqlash
+                merge_pdfs([muqova, test], f'camtest-{subject_name1}-{subject_name2}{num+1}.pdf')
+                os.remove(file_path)
 
-        revome_file(all_tests)
-        return Response({'status': 'success'})
+                book_code = data_muqova[1]
+                answer = AnswerTest(book_code=book_code, answers=full_answers[start_index:end_index])
+                answer.save()
+                start_index += 90
+                end_index += 90
+
+            # barcha generatsiya bo'lgan test fayllarini birlashtirish
+            test_book_name = generate_random_name()
+            merge_pdf(all_tests, f'/home/nazarbek/CamTest-admin/tests/{test_book_name}.pdf')
+            data = UserFile(user_id=user_name, file=f'{test_book_name}.pdf')
+            data.save()
+
+            revome_file(all_tests)
+            return Response({'success': True})
+        else:
+            return Response({'success': False, 'message': 'Sizda yetarlicha limit mavjud emas!'})
